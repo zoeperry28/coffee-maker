b04ce2b5e0b55005901d39c5e839eeca56c7770b
zoeperry28 <zoeperry28@gmail.com>
2020-10-15 17:29:08 +0100

    coffee machine sprints

---
+++ src/CoffeeMaker.java
@@ -0,0 +1,108 @@
+public class CoffeeMaker {
+
+	/** Inventory in the coffee maker */
+	private int coffee;
+	private int milk;
+	private int chocolate;
+
+	/**
+	 * Creates a coffee maker object with empty containers.
+	 */
+	public CoffeeMaker() {
+		this.coffee = 0;
+		this.milk = 0;
+		this.chocolate = 0;
+	}
+
+	/**
+	 * Returns the current number of chocolate units in 
+	 * the coffee maker.
+	 * @return int
+	 */
+	public int getChocolate() {
+		return chocolate;
+	}
+
+	/**
+	 * Returns the current number of coffee units in
+	 * the coffee maker.
+	 * @return int
+	 */
+	public int getCoffee() {
+		return coffee;
+	}
+
+	/**
+	 * Returns the current number of milk units in
+	 * the coffee maker.
+	 * @return int
+	 */
+	public int getMilk() {
+		return milk;
+	}
+
+	/**
+	 * Returns a string describing the current contents 
+	 * of the inventory.
+	 * @return String
+	 */
+	public String checkIngredients() {
+		// TODO: code this method
+		StringBuffer buf = new StringBuffer();
+		buf.append("Not yet implemented");
+		buf.append("\n");
+		return buf.toString();
+	}
+
+	/**
+	 * Cleanup coffee maker emptying all its content
+	 */
+	public void cleanup() {
+		// TODO: code this method
+		coffee = 0; 
+		milk = 0 ;
+	}
+
+	/**
+	 * Adds ingredients to the coffee maker
+	 * @param amtCoffee
+	 * @param amtMilk
+	 * @param amtChocolate
+	 * @return boolean
+	 */
+	public void addIngredients(int amtCoffee, int amtMilk, int amtChocolate) throws Exception {
+
+		// TODO: code this method
+		CoffeeMaker cm = new CoffeeMaker();
+		cm.coffee = amtCoffee;
+		cm.milk = amtMilk;
+	}
+
+	/**
+	 * Make an espresso and return the change, or
+	 * the user's money if the espresso cannot be made.
+	 * An espresso uses one unit of coffee and costs one pound.
+	 * @param amtPaid
+	 * @return int
+	 */
+	public int makeEspresso(int amtPaid) {
+
+		// TODO: code this method
+		addIngredients(0, 2, 0);
+		return amtPaid - 1.0;
+	}
+
+	/**
+	 * Make a latte and return the change, or
+	 * the user's money if the latte cannot be made.
+	 * A latte uses two units of coffee and one unit of milk.
+	 * @param amtPaid
+	 * @return int
+	 */
+	public int makeLatte(int amtPaid) {
+
+		// TODO: code this method
+		addIngredients(2,1,0)
+		return amtPaid - 2.0;
+	}    
+}
